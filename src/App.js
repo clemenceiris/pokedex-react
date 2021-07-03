04429d1d61429c8aa49d87bf8c8ee2007f5f29bf
Clemence Gazier <clemencegazier@MacBook-Pro-de-Clemence.local>
2021-07-03 18:55:33 +0200

    add style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-container">
       <h1>Pokemon Evolution</h1>
+      <div className="pokemon-container">
+        <div className="all-container">
+
+        </div>
+        <button className="load-more">Load more</button>
+      </div>
     </div>
   );
 }
